383adce13ebd1044040e83bd07aa475bb0a2b720
MelanX <info@melanx.de>
2026-04-19 10:49:02 +0200

    [meta] move TS to JS coremods

--- src/main/resources/coremods/cooking_for_blockheads.js
+++ src/main/resources/coremods/cooking_for_blockheads.js
@@ -1,6 +1,9 @@
-import {InsnList, MethodInsnNode, MethodNode, Opcodes, VarInsnNode} from "../coremods";
-
 function initializeCoreMod() {
+    var Opcodes = Java.type('org.objectweb.asm.Opcodes');
+    var VarInsnNode = Java.type('org.objectweb.asm.tree.VarInsnNode');
+    var MethodInsnNode = Java.type('org.objectweb.asm.tree.MethodInsnNode');
+    var InsnList = Java.type('org.objectweb.asm.tree.InsnList');
+
     return {
         'CfB': {
             'target': {
@@ -9,25 +12,19 @@ function initializeCoreMod() {
                 'methodName': 'serverTick',
                 'methodDesc': '(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/blay09/mods/cookingforblockheads/tile/FridgeBlockEntity;)V'
             },
-            'transformer': function (method: MethodNode) {
-                let target = new InsnList();
+            'transformer': function (method) {
+                const target = new InsnList();
+
                 target.add(new VarInsnNode(Opcodes.ALOAD, 0));
                 target.add(new VarInsnNode(Opcodes.ALOAD, 1));
                 target.add(new VarInsnNode(Opcodes.ALOAD, 2));
                 target.add(new VarInsnNode(Opcodes.ALOAD, 3));
-
-                target.add(new MethodInsnNode(
-                    Opcodes.INVOKESTATIC,
-                    'de/melanx/packessentials/compat/CookingForBlockheadsCompat',
-                    'fridgeMakesSnow',
-                    '(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/blay09/mods/cookingforblockheads/tile/FridgeBlockEntity;)V',
-                    false
-                ));
+                target.add(new MethodInsnNode(Opcodes.INVOKESTATIC, 'de/melanx/packessentials/compat/CookingForBlockheadsCompat', 'fridgeMakesSnow', '(Lnet/minecraft/world/level/Level;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/level/block/state/BlockState;Lnet/blay09/mods/cookingforblockheads/tile/FridgeBlockEntity;)V', false));
 
                 method.instructions.insert(target);
 
                 return method;
             }
         }
-    }
+    };
 }
